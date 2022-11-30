f5db08cb95f694af42327ff34c4f43418c61a2b6
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-12-01 00:55:03 +0100

    Searchbar

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<script setup>
+import { ref } from 'vue'
+
+
+
+</script>
+
+<template>
+  <div class="header-container">
+    <input type="text" placeholder="Inserisci un titolo...">
+  </div>
+</template>
+
+<style scoped>
+
+</style>
