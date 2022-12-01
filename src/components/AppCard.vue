1b70d044992c785ff87c0c57dbb1e64eb5c6a010
LorenzoMoccia <moccialorenzo98@gmail.com>
2022-12-01 17:18:40 +0100

    Milestone 1

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,7 +1,9 @@
 <template>
     <div>
         <h3>{{info.title}}</h3>
-
+        <p>{{info.original_title}}</p>
+        <p>{{info.original_language}}</p>
+        <p>{{info.vote_average}}</p>
     </div>
   </template>
   
